97f39fbe95d4a78b18bf7fa5d4027a6577663fcd
minhduy6868 <fbminhduyy4167@gmail.con>
2025-03-23 01:07:23 +0700

    rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Trắc nghiệm chọn nghề ',
+  description: 'Cùng làm trắc nghiệm tính cách để biết mình thuộc loại tính cách nào, chọn ngành nghề đúng',
+  generator: 'minh duyy nè!',
 }
 
 export default function RootLayout({
